cfe2c538ffaf8f2647fe383fee2db2f17ab8db8c
Łukasz Narloch <lukassz111@gmail.com>
2021-01-13 14:16:58 +0100

    fix folder name

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
-Remove-Item -Path ./public -Recurse
+Remove-Item -Path ./docs -Recurse
 Set-Location -Path ./page_src
 npm run build
 Set-Location -Path ./..
-Copy-Item -Path ./page_src/src/.vuepress/dist -Destination ./public -Recurse
+Copy-Item -Path ./page_src/src/.vuepress/dist -Destination ./docs -Recurse
